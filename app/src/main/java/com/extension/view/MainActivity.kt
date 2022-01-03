113273de774822c3ce797022f1dcc20a9e6a2231
jixiaoxin-5844 <cmin5844@gmail.com>
2022-01-03 11:54:39 +0800

    add lib_view

--- app/src/main/java/com/extension/view/MainActivity.kt
+++ app/src/main/java/com/extension/view/MainActivity.kt
@@ -2,10 +2,14 @@ package com.extension.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.extension.lib_view.TestMaven
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TestMaven(2)
+
     }
 }
---
+++ lib_view/src/main/java/com/extension/lib_view/TestMaven.kt
@@ -0,0 +1,5 @@
+package com.extension.lib_view
+
+class TestMaven(private val a: Int) {
+
+}
